9d0fb02ba3825957414f497a789ab5c6bc202d68
nonpro <nonprofessional0111@gmail.com>
2016-04-24 14:34:38 +0900

    ターゲットをcache

--- app.rb
+++ app.rb
@@ -4,8 +4,13 @@ require 'rest-client'
 require 'searchbing'
 require 'dalli'
 
-class App < Sinatra::Base
+dalli = Dalli::Client.new(ENV["MEMCACHIER_SERVERS"].split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"]
+                    })
+set :cache, dalli
 
+class App < Sinatra::Base
   def send_request(content_json)
     endpoint_uri = 'https://trialbot-api.line.me/v1/events'
     RestClient.proxy = ENV['FIXIE_URL'] if ENV['FIXIE_URL']
@@ -21,19 +26,38 @@ class App < Sinatra::Base
     params = JSON.parse(request.body.read)
 
     params['result'].each do |msg|
-      bing_image = Bing.new(ENV["BING_API_KEY"], 25, 'Image')
-      image_url = bing_image.search(msg['content']['text'])[0][:Image].sample[:MediaUrl]
+      message = msg['content']['text']
+      if message.match(/(.+)の画像/)
+        reply_text = "#{message[1]}の画像何枚欲しい？\n例) 「3枚」とか「3」って数字で答えてね!"
+        settings.cache.set("#{msg['content']['from']}:target", message[1], 600)
+      else
+        reply_text = "え？だれの画像？"
+      end
+
+#      bing_image = Bing.new(ENV["BING_API_KEY"], 25, 'Image')
+#      image_url = bing_image.search(msg['content']['text'])[0][:Image].sample[:MediaUrl]
+
       request_content = {
         to: [msg['content']['from']],
         toChannel: 1383378250,
         eventType: "138311608800106203",
         content: {
-          contentType: 2,
+          contentType: 1,
           toType: 1,
-          originalContentUrl: image_url,
-          previewImageUrl: image_url
+          text: settings.cache.get("#{msg['content']['from']}:target")
         }
       }
+#      request_content = {
+#        to: [msg['content']['from']],
+#        toChannel: 1383378250,
+#        eventType: "138311608800106203",
+#        content: {
+#          contentType: 2,
+#          toType: 1,
+#          originalContentUrl: image_url,
+#          previewImageUrl: image_url
+#        }
+#      }
       send_request(request_content.to_json)
     end
   end
